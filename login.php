b96333d6b1cf58ab581ad02cb9fddfac7c938204
Jasonwsc <jasonwsc@gmail.com>
2020-10-27 12:40:00 +0800

    Update login.php

--- login.php
+++ login.php
@@ -100,19 +100,9 @@ if (isset($_POST['username'])){
 		  color: dodgerblue;
 		}
 		
-	  .loader {
-	  border: 16px solid #f3f3f3;
-	  border-radius: 50%;
-	  border-top: 16px solid #3498db;
-	  width: 120px;
-	  height: 120px;
-	  -webkit-animation: spin 2s linear infinite; /* Safari */
-	  animation: spin 2s linear infinite;
-}
 	</style>
 </head>
 <body>
-	<div id="loader"></div>
     <h3>
     <?php
     echo $login_error;
