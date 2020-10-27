e1c18c2f733f6ee8845bc7b03ec31ae09dbf8712
bobross96 <zadus96@gmail.com>
2020-10-27 18:42:40 +0800

    allow user to update address, postal code and email in the profile page, added some basic regex check for address as well, not allow user to enter unless pass regex

--- javascript/checkout.js
+++ javascript/checkout.js
@@ -1,13 +1,33 @@
 //regex code to validate the delivery address
 
+confirmButton = document.getElementById('confirmPayment')
+
 
 function validateAdd(address){
-    console.log(address);
+    
+    var pos = address.search(/[^a-zA-Z\d\s\\#\-:]/)
+    if (pos >= 0){
+        alert(`Your address has invalid characters, please do not use special \n
+        characters in ur address`)
+        confirmButton.disabled = true;
+    }
+    else {
+        confirmButton.disabled = false;
+    }
+    
 }
 
 
 
 
 function validatePostalCode(postcode) {
-    console.log(postcode);
+    var pos = postcode.search(/^[0-9]{6}$/)
+    if (pos != 0){
+        alert(`please input postal code that is of 6 digits`)
+        confirmButton.disabled = true;
+    }
+
+    else {
+        confirmButton.disabled = false;
+    }
 }
--- profile.php
+++ profile.php
@@ -6,10 +6,26 @@ include 'dbconnect.php';
 
 $user_idINT = (int)$_SESSION['user_id'];
 
+
+if ( isset($_POST['update'])){
+    $newAddress = $_POST['addressInput'];
+    $newPostalCode = $_POST['postalCodeInput'];
+    $newEmail = $_POST['emailInput'];
+    $updateDB = "UPDATE users SET address = '$newAddress', postal_code = '$newPostalCode', email = '$newEmail' WHERE id = $user_idINT";
+    $db -> query($updateDB);
+
+    $userInfo = "SELECT * FROM users WHERE id = $user_idINT";
+    $result = $db->query($userInfo);
+    $row = $result -> fetch_assoc();
+
+}
+
+else {
 $userInfo = "SELECT * FROM users WHERE id = $user_idINT";
 $result = $db->query($userInfo);
 $row = $result -> fetch_assoc();
 
+}
 
 
 
@@ -24,6 +40,7 @@ $row = $result -> fetch_assoc();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Profile</title>
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="css/profile.css">
 </head>
 <body>
     
@@ -38,15 +55,18 @@ $row = $result -> fetch_assoc();
         <ul><a href="order_history.php">Order history</a></ul>
         </nav>
     </div>
+    <form action="" method='POST'>
     <div class="rightColumn">
         
         
             <p>User name: <?php echo $_SESSION['valid_user']; ?></p>
-            <p>Email:</p>
-            <p>Address: <?php echo $row['address']; ?> </p>
-        
-    
+            <p>Email: <input id="emailInput" name="emailInput" type="text" value="<?php echo $row['email']; ?>" readonly required><button type='button' id="emailEdit" >Edit</button></p> 
+            <p>Address: <input id="addressInput" name="addressInput" type="text" value="<?php echo $row['address']; ?>" readonly required><button type='button' id="addressEdit" >Edit</button></p> 
+            <p>Postal Code: <input id="postalCodeInput" name="postalCodeInput" type="text" value="<?php echo $row['postal_code']; ?>" readonly required><button type='button' id="postalCodeEdit">Edit</button></p> 
+            <button id="submitButton" type="submit" value="Update" name="update" style="display:none" >Update</button>
     </div>
+    </form>
     </div>
+    <script src="javascript/profile.js"></script>
 </body>
 </html>
