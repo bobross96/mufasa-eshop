acd27cd7925e6369f9b0c39c80ea889fb69b8d6e
bobross96 <zadus96@gmail.com>
2020-11-04 04:32:58 +0800

    added bg colour to admin login page

--- admin/index.php
+++ admin/index.php
@@ -6,13 +6,13 @@
     $login_error;
     $email = $_POST['email'];
     $password = md5($_POST['password']);
-   
+
 
     $sql = "SELECT * from admin_users WHERE email='$email' and password='$password'";
 
       $result = $db -> query($sql);
       $row = $result -> fetch_assoc();
-      
+
 
       //returns a query
       if ($result->num_rows>0){
@@ -23,17 +23,17 @@
           echo $row["id"];
           echo $_SESSION["user_id"];
           echo "<script>window.location.href='admin-stock.php'</script>";
-          
+
       }
 
       else {
           echo "$result->error_log";
           $login_error = 'Login failed. Please try again';
-          
+
       }
   }
 
-  
+
 
 
 
@@ -58,10 +58,16 @@
     <style>
         * {box-sizing: border-box}
 
+    body {
+      background-color: #bdd4e7;
+      background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);
+
+    }
+
     /* Add padding to containers */
     .container {
-      
-      text-align: center; 
+
+      text-align: center;
       position: absolute;
 			left: 50%;
 			top: 50%;
@@ -101,7 +107,7 @@
       cursor: pointer;
       width: 30%;
       opacity: 0.9;
-      
+
     }
 
     .loginbtn:hover {
@@ -116,15 +122,15 @@
     a {
       color: dodgerblue;
     }
-    
+
 </style>
-    
+
     <div class="container">
-        
+
         <h2>
         <?php
          echo $login_error;
-    
+
         ?>
 
         </h2>
@@ -134,16 +140,16 @@
         </div>
         <div style="text-align:center">
             <h2 style="font-style:italic">Login Page</h2>
-           
+
             <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-            
+
             <input id="email" type="text" name="email" placeholder="Email:" required onchange="validateEmail()"><br>
             <input type="password" name="password" placeholder="Password:" required><br>
             <button id="loginButton" name="submit" type="submit" class="loginbtn" value="Submit">Login</button>
             </form>
             </div>
     </div>
-    
+
     <script src="javascript/loginValidation.js"></script>
 </body>
 </html>
